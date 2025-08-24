d6ec8db79a7d3e324b75a87c7a8772bb779f26d0
D Santhosh Murthy <santhosh.murthy@games24x7.com>
2025-08-24 20:09:47 +0530

    Added spacy library

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import nltk
 import ssl
+import sys
+import subprocess
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -13,3 +15,12 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+
+# Download spaCy model
+try:
+    import spacy
+    if not spacy.util.is_package("en_core_web_sm"):
+        subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+except Exception as e:
+    print(f"Error downloading spaCy model: {str(e)}")
+    # Continue anyway as the model might be downloaded later
